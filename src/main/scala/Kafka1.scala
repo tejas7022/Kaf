1a88a56be256ea5a1b2f931fe38618b5c500d75a
tejas7022 <tejashwersai@gmail.com>
2021-04-26 11:13:45 +0530

    First Commit

---
+++ src/main/scala/Kafka1.scala
@@ -0,0 +1,12 @@
+import org.apache.spark
+import org.apache.spark.type.Stru
+
+object Kafka1 {
+  def main(args: Array[String]): Unit = {
+    val stockSchema =StructType(List(StructField))
+
+    val KafkaSourceDF=spark.re
+
+  }
+
+}
